cd5ef70923a6b0b6f372af694f7a9705be8bb77b
Jordan Ayers <adinsx3@gmail.com>
2022-01-16 19:21:36 -0800

    added test for custom structs

--- src/tests.rs
+++ src/tests.rs
@@ -6,6 +6,20 @@ fn const_primitive() {
 }
 
 #[test]
+fn const_custom() {
+    #[derive(Debug)]
+    struct TestStruct {
+        #[allow(dead_code)]
+        num: i32,
+    }
+    named!(const FIVE = (TestStruct (TestStruct{num: 5}), b"five"));
+    assert_eq!(
+        format!("{FIVE:?}"),
+        r#"easy_dst::Named<easy_dst::tests::const_custom::TestStruct>(TestStruct { num: 5 }, "five")"#
+    );
+}
+
+#[test]
 #[should_panic]
 fn invalid_byte_slice() {
     std::panic::set_hook(Box::new(|_| {}));
